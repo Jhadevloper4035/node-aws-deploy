aaac786292b33f83d842bba7944bcdd472efd278
Jhadevloper4035 <140846093+Jhadevloper4035@users.noreply.github.com>
2025-07-09 13:05:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Home Page!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Us Page');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Contact Us Here');
+});
+
+module.exports = app;
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+}
---
+++ index.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const app = require('./index');
+
+describe('Route Testing', () => {
+  test('GET / should return Home Page', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('Welcome to the Home Page!');
+  });
+
+  test('GET /about should return About Us', async () => {
+    const res = await request(app).get('/about');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('About Us Page');
+  });
+
+  test('GET /contact should return Contact Us', async () => {
+    const res = await request(app).get('/contact');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('Contact Us Here');
+  });
+});
