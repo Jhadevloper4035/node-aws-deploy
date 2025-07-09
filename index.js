e33b61c918b7db18fd42000d6f7816f88b63afc8
Jhadevloper4035 <140846093+Jhadevloper4035@users.noreply.github.com>
2025-07-09 13:33:53 +0530

    remove bug from index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config();
 
 
-const port = process.env.PORT
+const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Home Page!');
